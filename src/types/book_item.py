0093e0c91b1517bfa0891630e0df6253348af322
David Carpenter <carpenterd777@gmail.com>
2021-03-22 15:36:58 -0400

    Add item getters

--- src/types/book_item.py
+++ src/types/book_item.py
@@ -2,7 +2,7 @@ from decimal import Decimal
 
 from item import Item
 
-class Book(Item):
+class BookItem(Item):
 
     def __init__(self, item_id: int,
                  title: str,
@@ -14,11 +14,13 @@ class Book(Item):
                  edition: int,
                  course_number: int):
         super().__init__(item_id, title, desc, price, weight, seller)
-        self._book_title = book_title
-        self._edition = edition
-        self._course_number = course_number
 
-    def get_book_title(self): -> str:
+        # Private instance attributes
+        self._book_title: str    = book_title
+        self._edition: int       = edition
+        self._course_number: int = course_number
+
+    def get_book_title(self) -> str:
         return self._book_title
 
     def get_edition(self) -> int:
--- src/types/item.py
+++ src/types/item.py
@@ -1,4 +1,5 @@
 from decimal import Decimal
+from typing import List
 
 class Item:
     """
@@ -7,47 +8,47 @@ class Item:
 
     def __init__(self, item_id: int, title: str, desc: str, price: Decimal, weight: float, seller: str):
         # Private instance attributes
-        self._item_id = item_id
-        self._title = title
-        self._description = desc
-        self._price = price
-        self._weight = weight
-        self._seller = seller
-        self._condition = ""
+        self._item_id: int     = item_id
+        self._title: str       = title
+        self._description: str = desc
+        self._price: Decimal   = price
+        self._weight: float    = weight
+        self._seller: str      = seller
+        self._condition: str   = ""
 
         # Public instance attributes
-        self.quantity = 1
+        self.quantity: int = 1
 
-    def get_item_id(self): -> int:
+    def get_item_id(self) -> int:
         return self._item_id
 
-    def get_title(self): -> str:
+    def get_title(self) -> str:
         return self._title
 
-    def get_description(self): -> str:
+    def get_description(self) -> str:
         return self._description
 
-    def get_price(self): -> Decimal:
+    def get_price(self) -> Decimal:
         return self._price
 
-    def get_weight(self): -> float:
+    def get_weight(self) -> float:
         return self._weight
 
-    def get_seller(self): -> str:
+    def get_seller(self) -> str:
         return self._seller
 
-    def get_condition(self): -> str:
+    def get_condition(self) -> str:
         return self._condition
 
     @classmethod
-    def get_all() -> list[Item]:
+    def get_all(cls) -> List[Item]:
         pass
 
-    @clasmethod
+    @classmethod
     def get_item_by_id(cls, item_id: int) -> Item:
         pass
 
     @classmethod
-    def get_item_by_catergory(cls, category: str) -> list[Item]:
+    def get_item_by_catergory(cls, category: str) -> List[Item]:
         pass
 
