d23be75037b8b8fde386ce2a4906628b4ca09852
SWiessler <s.wiessler@comcast.net>
2021-03-31 11:38:29 -0400

    Some form work

--- src/app.py
+++ src/app.py
@@ -1,7 +1,24 @@
+import os
 from flask import Flask, render_template
+from flask_uploads import UploadSet, configure_uploads, IMAGES, patch_request_class
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms import SubmitField
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'Howdy D6'
+app.config['UPLOADED_PHOTOS_DEST'] = os.path.join(basedir, 'uploads') # you'll need to create a folder named uploads
+
+photos = UploadSet('photos', IMAGES)
+configure_uploads(app, photos)
+patch_request_class(app)  # set maximum file size, default is 16MB
+
+class UploadForm(FlaskForm):
+    photo = FileField(validators=[FileAllowed(photos, 'Image only!'), FileRequired('File was empty!')])
+    submit = SubmitField('Upload')
 
 @app.route('/')
 def get_home():
@@ -33,9 +50,15 @@ def get_furniture():
     return render_template('/furniture.html')
 
 
-@app.route('/sell')
+@app.route('/sell', methods=['GET', 'POST'])
 def get_sell():
-    return render_template('/sell.html')
+    form = UploadForm()
+    if form.validate_on_submit():
+        filename = photos.save(form.photo.data)
+        file_url = photos.url(filename)
+    else:
+        file_url = None
+    return render_template('/sell.html', form=form, file_url=file_url)
 
 
 @app.route('/view')
