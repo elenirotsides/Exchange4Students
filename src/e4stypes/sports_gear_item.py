b21bc8c66c857d7c78bd176855913d5cbb0aae5f
David Carpenter <carpenterd777@gmail.com>
2021-04-02 00:37:29 -0400

    changed size field of sports gear item to clothing size

--- src/e4stypes/sports_gear_item.py
+++ src/e4stypes/sports_gear_item.py
@@ -2,12 +2,12 @@ from decimal import Decimal
 from typing import List
 
 from item import Item
-from clothing_item import ClothingGender
+from clothing_item import ClothingGender, ClothingSize
 
 
 class SportsGearItem(Item):
     def __init__(self, item_id: str, title: str, desc: str, price: Decimal,
-                 weight: float, seller: str, gear_type: str, size: str,
+                 weight: float, seller: str, gear_type: str, size: ClothingSize,
                  gender: ClothingGender):
         super().__init__(item_id, title, desc, price, weight, seller)
 
@@ -19,7 +19,7 @@ class SportsGearItem(Item):
     def get_gear_type(self) -> str:
         return self._gear_type
 
-    def get_size(self) -> str:
+    def get_size(self) -> ClothingSize:
         return self._size
 
     def get_gender(self) -> List[int]:
