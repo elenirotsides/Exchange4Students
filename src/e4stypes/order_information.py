c2451037eb70f613b4ea64df39cd8f94939d1f04
jlora23 <48076390+jlora23@users.noreply.github.com>
2021-04-30 20:18:41 -0400

    Disallow adding same item to cart

--- src/e4stypes/order_information.py
+++ src/e4stypes/order_information.py
@@ -2,6 +2,7 @@ from decimal import Decimal
 from typing import List
 from .database import Database
 from .item import Item
+from bson import ObjectId
 
 
 class OrderInformation:
@@ -11,7 +12,11 @@ class OrderInformation:
 
     def add_to_cart(self, item_id: str) -> type(None):
         item = Database.get_item_by_id(item_id)
-        if item not in self.item_list:
+        item_list_ids = []
+        for item1 in self.item_list:
+            item_list_ids.append(item1.get_item_id())
+        print(item_list_ids)
+        if ObjectId(item_id) not in item_list_ids:
             self.item_list.append(item)
 
     def remove_from_cart(self, item_id: str) -> type(None):
