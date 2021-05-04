62572f7d264a7d9e2dd2e1144053ce8b563bbbf4
Eleni Rotsides <43190242+elenirotsides@users.noreply.github.com>
2021-05-04 18:19:45 -0400

    update is_sold

--- src/e4stypes/database.py
+++ src/e4stypes/database.py
@@ -304,14 +304,24 @@ class Database:
     @classmethod
     def update_sold(cls, item) -> type(None):
         if isinstance(item, ClothingItem):
-            clothing_col.update_one({"_id": item._id}, {"$set": {'is_sold': True}})
+            clothing_col.update_one(
+                {"_id": item.get_item_id()}, {"$set": {"is_sold": True}}
+            )
         elif isinstance(item, BookItem):
-            book_col.update_one({"_id": item._id}, {"$set": {'is_sold': True}})
+            book_col.update_one(
+                {"_id": item.get_item_id()}, {"$set": {"is_sold": True}}
+            )
         elif isinstance(item, FurnitureItem):
-            furniture_col.update_one({"_id": item._id}, {"$set": {'is_sold': True}})
+            furniture_col.update_one(
+                {"_id": item.get_item_id()}, {"$set": {"is_sold": True}}
+            )
         elif isinstance(item, ElectronicItem):
-            electronic_col.update_one({"_id": item._id}, {"$set": {'is_sold': True}})
+            electronic_col.update_one(
+                {"_id": item.get_item_id()}, {"$set": {"is_sold": True}}
+            )
         elif isinstance(item, SportsGearItem):
-            sports_gear_col.update_one({"_id": item._id}, {"$set": {'is_sold': True}})
+            sports_gear_col.update_one(
+                {"_id": item.get_item_id()}, {"$set": {"is_sold": True}}
+            )
         else:
             raise RuntimeError("Could not update item")
